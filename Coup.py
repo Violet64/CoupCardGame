edeeb7c73843cec035d61b2e289180d39281cf47
Violet <crystalbomb64@hotmail.com>
2022-04-03 02:41:46 +0400

    Added card class with basic attributes and basic constructor for Coup class

--- Coup.py
+++ Coup.py
@@ -1,3 +1,38 @@
-#Main Coup game class
+# Main Coup game class and cards
+
+
+class Card:
+    def __init__(self, art, steal, block_steal, block_kill, kill, tax, swap, inquisitor):
+        self.art = art
+        self.steal = steal
+        self.block_steal = block_steal
+        self.block_kill = block_kill
+        self.kill = kill
+        self.tax = tax
+        self.swap = swap
+        self.inquisitor = inquisitor
+
+
 class Coup:
-  pass
+    def __init__(self, players, inquisitor: bool, ambassador: bool, contessa: bool, timer):
+        self.players = players
+        self.timer = timer
+        self.cards = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]
+        # checking custom game rules
+        if inquisitor is True:
+            self.inquisitor = Card(1, False, True, False, False, False, False, True)
+            self.cards.append(5)
+            self.cards.append(5)
+            self.cards.append(5)
+        if ambassador is True:
+            self.ambassador = Card(1, False, False, True, False, False, True, False)
+        else:
+            self.ambassador = Card(1, False, True, False, False, False, True, False)
+        if contessa is True:
+            self.contessa = Card(1, False, True, True, False, False, False, False)
+        else:
+            self.contessa = Card(1, False, False, True, False, False, False, False)
+
+        self.captain = Card(1, True, True, False, False, False, False, False)
+        self.assassian = Card(1, False, False, False, True, False, False, False)
+        self.duke = Card(1, False, False, False, False, True, False, False)
