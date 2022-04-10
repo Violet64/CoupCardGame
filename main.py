0ef4a87980ef717a613093396e52fd30fe788067
Violet <crystalbomb64@hotmail.com>
2022-04-10 22:44:50 +0400

    Added a slash command to start a new game, and a button to add new players to the game

--- main.py
+++ main.py
@@ -1,12 +1,66 @@
-#This file is for handling interaction with the discord API through the interactions module
+# This file is for handling interaction with the discord API through the interactions module
 import interactions
-from getpass import getuser
+from Coup import Coup
+from asyncio import sleep
 
-with open(f"C:\\Users\\{getuser()}\\Documents\\bot_token.txt", "r") as file:
+with open(f"bot_token.txt", "r") as file:
     for line in file:
         token = line
 
 bot = interactions.Client(token=token)
+game: Coup
+player_list = []
 
+@bot.command(
+    name="start_game",
+    description="fun for the whole familia",
+    options=[
+        interactions.Option(
+            name="players",
+            description="Number of players",
+            type=interactions.OptionType.INTEGER,
+            required=True,
+            min_value=1,
+            max_value=4
+        ),
+        interactions.Option(
+            name="inquisitor",
+            description="Add inquisitor y/n",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+        interactions.Option(
+            name="ambassador",
+            description="Use modified Ambassador y/n",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+        interactions.Option(
+            name="contessa",
+            description="Use modified Contessa y/n",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
+)
+async def new_game(ctx: interactions.CommandContext, players, inquisitor, ambassador, contessa):
+    global game
+    joined = 0
+    join = interactions.Button(
+        style=interactions.ButtonStyle.PRIMARY,
+        label="Join game",
+        custom_id="joined"
+    )
+    await ctx.send("", components=join)
+    await sleep(10)
+    game = Coup(players, inquisitor, ambassador, contessa, player_list)
+
+
+@bot.component("joined")
+async def join_game(ctx: interactions.CommandContext):
+    await ctx.send(f"{ctx.author.user.mention} joined")
+    player_list.append(ctx.author)
 
 bot.start()
+
+
