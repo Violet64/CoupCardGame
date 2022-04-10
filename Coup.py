bdc36e8c6b1f5c0e06818f8e31b8f4b57ea879f4
Violet <crystalbomb64@hotmail.com>
2022-04-10 22:46:45 +0400

    Minor changes to Coup class to line up with the new slash command in main.py (mainly removed timer argument and added the player id list argument)

--- Coup.py
+++ Coup.py
@@ -17,38 +17,39 @@ class Card:
 
 
 class Coup:
-    def __init__(self, players, inquisitor: bool, ambassador: bool, contessa: bool, timer):
+    def __init__(self, players, inquisitor, ambassador, contessa, player_list):
         self.player_num = players
-        self.timer = timer
         self.turn = 1
         self.cards = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]
         # checking custom game rules
-        if inquisitor is True:
+        if inquisitor == "y":
             self.inquisitor = Card(1, "Inquisitor", False, True, False, False, False, False, True)
             self.cards.append(5)
             self.cards.append(5)
             self.cards.append(5)
         else:
             self.inquisitor = None
-        if ambassador is True:
+        if ambassador == "y":
             self.ambassador = Card(1, "Ambassador", False, False, True, False, False, True, False)
         else:
-            self.ambassador = Card(1, "Ambassador",False, True, False, False, False, True, False)
-        if contessa is True:
-            self.contessa = Card(1, "Contessa",False, True, True, False, False, False, False)
+            self.ambassador = Card(1, "Ambassador", False, True, False, False, False, True, False)
+        if contessa == "y":
+            self.contessa = Card(1, "Contessa", False, True, True, False, False, False, False)
         else:
-            self.contessa = Card(1, "Contessa",False, False, True, False, False, False, False)
+            self.contessa = Card(1, "Contessa", False, False, True, False, False, False, False)
 
-        self.captain = Card(1, "Captain",True, True, False, False, False, False, False)
+        self.captain = Card(1, "Captain", True, True, False, False, False, False, False)
         self.assassian = Card(1, "Assassian", False, False, False, True, False, False, False)
-        self.duke = Card(1, "Duke",False, False, False, False, True, False, False)
+        self.duke = Card(1, "Duke", False, False, False, False, True, False, False)
 
         self.card_map = {0: self.ambassador, 1: self.contessa, 2: self.captain, 3: self.assassian, 4: self.duke,
                          5: self.inquisitor}
 
-        self.players = [Player(self.card_map[self.add_card()], self.card_map[self.add_card()])
+        # simple list comprehension to create all player objects with random cards
+        self.players = [Player(self.card_map[self.add_card()], self.card_map[self.add_card()], player_list[i])
                         for i in range(self.player_num)]
 
+    # to pick a card from the deck and return it
     def add_card(self):
         card = choice(self.cards)
         self.cards.remove(card)
