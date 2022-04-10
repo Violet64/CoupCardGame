303f7822063bd52354151e65cd322e8d32bc030f
Violet <crystalbomb64@hotmail.com>
2022-04-10 22:45:32 +0400

    added an id in the player class to identify which user it belongs to (and added a coin count)

--- Player.py
+++ Player.py
@@ -1,11 +1,11 @@
 # Individual player class
-
-
 class Player:
-    def __init__(self, card1, card2):
+    def __init__(self, card1, card2, name):
         self.cards = [card1, card2]
         self.health = 2
+        self.name = name
+        self.coins = 2
 
     # testing functions (temporary)
     def print(self):
-        print(self.cards[0].name)
+        print(self.name.id)
