36cab0016d3fadc56794f9a225baa39ed859b6e9
Violet <crystalbomb64@hotmail.com>
2022-04-12 01:52:56 +0400

    renamed name attribute to id to better reflect the data it holds

--- Player.py
+++ Player.py
@@ -3,9 +3,9 @@ class Player:
     def __init__(self, card1, card2, name):
         self.cards = [card1, card2]
         self.health = 2
-        self.name = name
+        self.id = name
         self.coins = 2
 
     # testing functions (temporary)
     def print(self):
-        print(self.name.id)
+        print(self.id)
