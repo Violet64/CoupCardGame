b107043c969b9b56a4fdfbdee9c3d76c6b5e18b9
Violet <crystalbomb64@hotmail.com>
2022-04-12 01:54:02 +0400

    1. Refined the join game functionality to allow you to enter once only 2. Added 2 basic gameplay functions to gain income during your turn and display the balance of any user

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ bot = interactions.Client(token=token)
 game: Coup
 player_list = []
 
+
 @bot.command(
     name="start_game",
-    description="fun for the whole familia",
+    description="Start a new game",
     options=[
         interactions.Option(
             name="players",
@@ -45,7 +46,6 @@ player_list = []
 )
 async def new_game(ctx: interactions.CommandContext, players, inquisitor, ambassador, contessa):
     global game
-    joined = 0
     join = interactions.Button(
         style=interactions.ButtonStyle.PRIMARY,
         label="Join game",
@@ -58,8 +58,33 @@ async def new_game(ctx: interactions.CommandContext, players, inquisitor, ambass
 
 @bot.component("joined")
 async def join_game(ctx: interactions.CommandContext):
-    await ctx.send(f"{ctx.author.user.mention} joined")
-    player_list.append(ctx.author)
+    if str(ctx.author.id) not in player_list:
+        await ctx.send(f"{ctx.author.user.mention} joined")
+        player_list.append(str(ctx.author.id))
+    else:
+        await ctx.send("You are already in the game")
+
+
+@bot.command(
+    name="income",
+    description="Gain 1 coin",
+)
+async def gain_income(ctx: interactions.CommandContext):
+    if game.check_turn(ctx.author.id):
+        await ctx.send(f"{ctx.author.user.mention} gained 1 coin")
+        game.income()
+        game.next_turn()
+    else:
+        await ctx.send("It is not your turn yet")
+
+
+@bot.command(
+    type=interactions.ApplicationCommandType.USER,
+    name="balance",
+)
+async def check_balance(ctx: interactions.CommandContext):
+    await ctx.send(f"{ctx.target.user.username} has {game.get_balance(ctx.target.id)} coins")
+
 
 bot.start()
 
